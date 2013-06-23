f2522632e777ba712caf18550abde1bb727ea937
style513 <86799138@qq.com>
2013-06-23 10:29:54 +0800

    test

---
+++ 2dx/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  _dxAppController.h
+//  2dx
+//
+//  Created by yons on 13-6-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
